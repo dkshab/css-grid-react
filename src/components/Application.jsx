e760401ae757f766c7d33403b6960d08b316c628
Dumisani <dumisanik.shabangu@gmail.com>
2020-03-26 09:23:16 +0200

    Flexbox Grid with Breakpoints

---
+++ src/components/Application.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import FlexboxGrid from "./FlexboxGrid";
+
+const Application = () => {
+  return (
+    <div>
+      <FlexboxGrid />
+    </div>
+  );
+};
+
+export default Application;
---
+++ src/components/FlexboxGrid.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const FlexboxGrid = () => {
+  return (
+    <div className="container">
+      <header className="header column">header</header>
+      <main className="content column">content</main>
+      <aside className="sidebar column">sidebar</aside>
+    </div>
+  );
+};
+
+export default FlexboxGrid;
